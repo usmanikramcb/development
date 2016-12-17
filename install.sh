051129ed8fd7c26069e526b8dff3f6cafec7fc39
Niels Gerritsen <n.c.gerritsen@gmail.com>
2016-12-17 14:21:37 +0100

    Add yarn and n

--- install.sh
+++ install.sh
@@ -31,6 +31,9 @@ boot2docker init
 boot2docker up
 
 npm install -g http-server
+npm install -g n
+npm install -g yarn
+npm install -g gulp
 
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('SHA384', 'composer-setup.php') === 'e115a8dc7871f15d853148a7fbac7da27d6c0030b848d9b3dc09e2a0388afed865e6a3d6b3c0fad45c48e2b5fc1196ae') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
