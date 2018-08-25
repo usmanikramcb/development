32611e6cbb06719371f9e9683e9d86761596c5bd
Niels Gerritsen <n.c.gerritsen@gmail.com>
2018-08-25 11:20:39 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+GREEN='\033[0;32m'
+WHITE='\033[0m'
+
 echo "\nUpdating config files..."
 
 cp .bash_profile ~
 cp .gitconfig ~
+cp .gitignore_global ~
 cp .vimrc ~
-cp -r .atom/* ~/.atom
+cp .zshrc ~
 
 echo "${GREEN}All done!${WHITE}\n"
